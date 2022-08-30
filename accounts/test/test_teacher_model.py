dfb251b7e290e768e453413f1eec2a39103f85df
Ahzam Ahmed <ahzamahmed6@gmail.com>
2022-08-30 22:56:47 +0500

    write tests for teacher model and registeration endpoint

---
+++ accounts/test/test_teacher_model.py
@@ -0,0 +1,20 @@
+import pytest
+from accounts.models import Teacher
+
+pytestmark = pytest.mark.django_db
+
+
+def test_no_email_exception():
+    with pytest.raises(ValueError) as er:
+        Teacher.objects.create_user(name='Teacher')
+    assert "User must have an email address" == str(er.value)
+
+
+def test_create_account():
+    Teacher.objects.create_user(
+        name='Teacher', email='teacher@test.com', password='1234')
+    data = Teacher.objects.first()
+    assert data.name == 'Teacher'
+    assert data.email == 'teacher@test.com'
+    assert data.check_password('1234')
+    assert data.type == 'TEACHER'
---
+++ accounts/test/test_teacher_registeration.py
@@ -0,0 +1,85 @@
+import json
+
+import pytest
+from accounts.models import Admin
+from django.urls import reverse
+
+
+url = reverse('Teacher_Register')
+pytestmark = pytest.mark.django_db
+
+FIELD_REQUIRED_MESSAGE = {
+    "email": [
+        "This field is required."
+    ],
+    "name": [
+        "This field is required."
+    ],
+    "password": [
+        "This field is required."
+    ],
+    "password2": [
+        "This field is required."
+    ]
+}
+
+
+def test_get_zero_content(client):
+    response = client.post(url)
+    response_content = json.loads(response.content)
+    assert response.status_code == 400
+    assert response_content == FIELD_REQUIRED_MESSAGE
+
+
+def test_wrong_confirm_password(client):
+    data = {
+        'name': 'Admin', 'email': 'teacher@test.com', 'password': '1234',
+        'password2': '12345'}
+    response = client.post(url, data)
+    response_content = json.loads(response.content)
+    assert response.status_code == 400
+    assert response_content == {
+        "non_field_errors": [
+            "Password and Confirm Password doesn't match"
+        ]
+    }
+
+
+def test_with_same_email(client):
+    Admin.objects.create(name='Admin', email='teacher@test.com')
+    response = client.post(url, {'name': 'Admin', 'email': 'teacher@test.com',
+                                 'password': '1234', 'password2': '1234'})
+    response_content = json.loads(response.content)
+    assert response.status_code == 400
+    assert response_content == {
+        "email": [
+            "user with this Email already exists."
+        ]
+    }
+
+
+def test_with_wrong_data(client):
+    response = client.post(url, {'name': 'Admin', 'email': 'teachertest.com',
+                                 'password': '1234', 'password2': '1234'})
+    response_content = json.loads(response.content)
+    assert response.status_code == 400
+    assert response_content == {
+        "email": [
+            "Enter a valid email address."
+        ]
+    }
+
+
+def test_registeration_success(client):
+    data = {
+        'name': 'Admin',
+        'email': 'teacher@example.com',
+        'password': '1234',
+        'password2': '1234'
+    }
+    response = client.post(url, data)
+    response_content = json.loads(response.content)
+    assert response.status_code == 201
+    assert response_content == {
+        "msg": "Registeration Success"
+    }
