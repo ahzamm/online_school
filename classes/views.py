b205d58fd782a11ebdc9bc378ca3b075ff5d7c2a
Ahzam Ahmed <ahzamahmed6@gmail.com>
2022-09-11 14:17:35 +0500

    create doto list

--- classes/views.py
+++ classes/views.py
@@ -1,3 +1,9 @@
 # from django.shortcuts import render
 
 # Create your views here.
+
+# TODO
+# - admin can create Course
+# - admin can create TimeTable
+# - Teacher can create class
+# - Teacher can create and insert Attendence
