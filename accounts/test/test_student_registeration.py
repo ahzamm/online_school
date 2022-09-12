61baec0e59ed28715f64040d9ae2a129d86f09b9
Ahzam Ahmed <ahzamahmed6@gmail.com>
2022-09-13 03:14:01 +0500

    clean code

--- accounts/test/test_student_registeration.py
+++ accounts/test/test_student_registeration.py
@@ -6,14 +6,14 @@ from accounts.messages import *
 from accounts.models import Student
 from django.urls import reverse
 
-from .extra import FIELD_REQUIRED_MESSAGE, non_field_error
+from .extra import DUMMY_TOKEN, FIELD_REQUIRED_MESSAGE, non_field_error
 
 url = reverse('Student_Register')
 pytestmark = pytest.mark.django_db
 
 
 DATA = {'name': 'Student', 'email': 'student@test.com', 'password': '1234',
-        'password2': '12345'}
+        'password2': '1234'}
 
 
 def test_get_zero_content(client, create_test_admin):
@@ -71,25 +71,15 @@ def test_with_wrong_data(client, create_test_admin):
 
 @patch('accounts.views.get_tokens_for_user')
 def test_registeration_success(patch_token, client, create_test_admin):
-    patch_token.return_value = {
-        "refresh": "DummyRefreshToken",
-        "access": "DummyAccessToken"
-    }
-    data = {
-        'name': 'Student',
-        'email': 'student@example.com',
-        'password': '1234',
-        'password2': '1234'
-    }
+    patch_token.return_value = DUMMY_TOKEN
+
     token = create_test_admin
     response = client.post(
-        url, data, **{'HTTP_AUTHORIZATION': f'Bearer {token}'})
+        url, DATA, **{'HTTP_AUTHORIZATION': f'Bearer {token}'})
     response_content = json.loads(response.content)
-    assert response.status_code == 201
+
     assert response_content == {
-        "msg": "Registeration Success",
-        "token": {
-            "refresh": "DummyRefreshToken",
-            "access": "DummyAccessToken"
-        }
+        "msg": REGISTERATION_SUCCESS_MESSAGE,
+        "token": DUMMY_TOKEN
     }
+    assert response.status_code == 201
